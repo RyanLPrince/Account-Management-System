3d2594dfb1acef2b29c817f5066ed6432d551da6
RyanLPrince <37381046+RyanLPrince@users.noreply.github.com>
2018-04-25 13:33:47 +0100

    wrote test for creating an account

--- src/test/java/com/qa/business/AccountDBImpTest.java
+++ src/test/java/com/qa/business/AccountDBImpTest.java
@@ -2,11 +2,16 @@ package com.qa.business;
 
 import static org.junit.Assert.*;
 
+import javax.persistence.EntityManager;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import com.qa.domain.Account;
+import com.qa.util.JSONUtil;
 
 
 
@@ -16,10 +21,24 @@ public class AccountDBImpTest {
 	@InjectMocks
 	AccountImp accImp= new AccountDBImp();
 	
+	@Mock 
+	private Account account;
+	
+	@Mock 
+	private EntityManager em;
+	
+	@Mock 
+	private JSONUtil jsonutil;
+	
+	private static final String MockAccount = "{\"firstName\":\"Ryan\",\"secondName\":\"Prince\",\"accountNumber\":\"1\"}";
+	private static final String MockAccountList = "[{\"firstName\":\"Ryan\",\"secondName\":\"Prince\",\"accountNumber\":\"1\"}]";
+	
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createAccountTest() {
+		String expectedValue="{\"message\": \"account has been created.\"}";
+		String actualValue= accImp.createAccount(MockAccount);
+		assertEquals(expectedValue,actualValue);
 	}
 
 }
